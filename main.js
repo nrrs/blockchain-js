ca5be116cc24ca65d2a9773e886c3f5e24d3c051
Norris Kwan <norris.kwan@gmail.com>
2017-11-20 17:13:34 -0800

    Add Block and Blockchain class

--- main.js
+++ main.js
@@ -1,13 +1,41 @@
-const SHA256 = require('crypto-js');
+const SHA256 = require('crypto-js/sha256');
 
 class Block {
     constructor(index, timestamp, data, previousHash = '') {
         this.index = index;
         this.timestamp = timestamp;
+        this.previousHash = previousHash;
         this.data = data;
-        this. previousHash = previousHash;
-        this.hash = '';
+        this.hash = this.calculateHash();
     }
 
-    calculateHash() {}
+    calculateHash() {
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
 }
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        return new Block(0, "11/20/2017", "Genesis Block", "0");
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
+
+let norrisCoin = new Blockchain();
+norrisCoin.addBlock(new Block(1, "11/20/2017", { amount: 5 }));
+norrisCoin.addBlock(new Block(2, "11/21/2017", { amount: 10 }));
+
+console.log(JSON.stringify(norrisCoin, null, 4));
