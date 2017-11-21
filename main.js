ada0339940e9deeec88291232d9db347d6f0a1c7
Norris Kwan <norris.kwan@gmail.com>
2017-11-20 17:16:40 -0800

    Add blockchain validation

--- main.js
+++ main.js
@@ -32,10 +32,30 @@ class Blockchain {
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    isChainValid() {
+        for(let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
 
 let norrisCoin = new Blockchain();
 norrisCoin.addBlock(new Block(1, "11/20/2017", { amount: 5 }));
 norrisCoin.addBlock(new Block(2, "11/21/2017", { amount: 10 }));
 
-console.log(JSON.stringify(norrisCoin, null, 4));
+console.log('Is blockchain valid?', norrisCoin.isChainValid());
+
+
+// console.log(JSON.stringify(norrisCoin, null, 4));
