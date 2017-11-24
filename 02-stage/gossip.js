518ae764ccfccf6ddbc0d68305fbbf741145d085
Norris Kwan <norris.kwan@gmail.com>
2017-11-23 20:43:53 -0800

    Prettify

--- 02-stage/gossip.js
+++ 02-stage/gossip.js
@@ -1,65 +1,56 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// import Client from './client';
-// const helpers = require('./helpers');
+const axios = require("axios");
 
 const app = express();
 
-const PORT = process.argv[2],
-    PEER_PORT = process.argv[3];
-
-const STATE = {};
-
-const axios = require("axios");
-
 const URL = "http://localhost";
-
-class Client {
-  constructor() {}
-  gossip(port, state) {
-    if (port === PORT) {
-      return JSON.stringify({});
-    }
-
-    axios
-      .post(`${URL}:${port}/gossip`, { state })
-      .then(res => {
-        console.log("Client post SUCCESS");
-        return res.data;
-      })
-      .catch(err => console.log("Client post FAIL"));
-  }
-}
-
-updateState({[PORT]: null});
-updateState({[PEER_PORT]: null});
-// helpers.updateState({[PORT]: null});
-// helpers.updateState({[PEER_PORT]: null});
+const PORT = process.argv[2];
+const PEER_PORT = process.argv[3];
 
 const ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+const STATE = {};
 
 Array.prototype.random = function () {
     return this[Math.floor(Math.random() * this.length)];
 };
+
+class Client {
+    constructor() {}
+    gossip(port, state) {
+        if (port === PORT) {
+            return JSON.stringify({});
+        }
+
+        axios
+            .post(`${URL}:${port}/gossip`, {state})
+            .then(res => {
+                console.log("Client post SUCCESS");
+                return res.data;
+            })
+            .catch(err => console.log("Client post FAIL"));
+    }
+}
+
+updateState({[PORT]: null});
+updateState({[PEER_PORT]: null});
 
 let faveLetter = ALPHA.random();
 let versionNum = 0;
 
 console.log(`My favorite letter is ${faveLetter}\n`);
 
-
 updateState({
     [PORT]: [faveLetter, versionNum]
 });
-// helpers.updateState({
-//     [PORT]: [faveLetter, versionNum]
-// });
 
-// Change state every 8 seconds
+// Change fave letter every 8 seconds
 setInterval(() => {
     faveLetter = ALPHA.random();
     versionNum++;
-    updateState({[PORT]: [faveLetter, versionNum]});
+    updateState({
+        [PORT]: [faveLetter, versionNum]
+    });
     console.log('\n');
     console.log("!".repeat(40));
     console.log(`Nevermind, my new favorte letter is ${faveLetter}`);
@@ -68,20 +59,20 @@ setInterval(() => {
 
 // Fetch updates every 3 secs
 setInterval(() => {
-    Object.keys(STATE)
+    Object
+        .keys(STATE)
         .forEach(port => {
             if (port === PORT) {
                 return;
             }
             console.log(`\nFetching update from ${port}`);
-            
+
             let gossipResponse = new Client();
             gossipResponse = gossipResponse.gossip(port, STATE);
-            // gossipResponse = gossipResponse.gossip(port, JSON.stringify(STATE));
             updateState(gossipResponse);
         });
-    
-        renderState();
+
+    renderState();
 }, 1000);
 
 // Helpers
@@ -100,13 +91,15 @@ function updateState(update) {
 
 function renderState() {
     console.log('-'.repeat(40));
-    Object.keys(STATE).forEach(port => {
-        if (STATE[port] === null) {
-            console.log(`${port} doesn't have a fave letter yet`);
-        } else {
-            console.log(`${port} fave letter is ${STATE[port][0]}`);
-        }
-    });
+    Object
+        .keys(STATE)
+        .forEach(port => {
+            if (STATE[port] === null) {
+                console.log(`${port} doesn't have a fave letter yet`);
+            } else {
+                console.log(`${port} fave letter is ${STATE[port][0]}`);
+            }
+        });
     console.log('-'.repeat(40));
 }
 
